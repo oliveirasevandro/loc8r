7c34deb26931ece57964a72058c1b863ce0dda85
Evandro Oliveira <eoliveira@kenzan.com>
2016-07-11 11:09:49 -0300

    Passing data from controller to views

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -7,7 +7,7 @@ module.exports.homelist = function(req, res) {
         title: 'Loc8r',
         strapline: 'Find places to work with wifi near you!'
       },
-      sidebar: 'Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you´re looking for.',
+      sidebar: 'Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you\'re looking for.',
       locations: [{
         name: 'Starcups',
         address: '125 High Street, Reading, RG6 1PS',
@@ -32,10 +32,56 @@ module.exports.homelist = function(req, res) {
 
 /* Get Location Info Page */
 module.exports.locationInfo = function(req, res) {
-  res.render('location-info', { title: 'Location Info' });
+  res.render('location-info', {
+    title: 'Location Info',
+    pageHeader: {
+      title: 'Starcups'
+    },
+    sidebar: {
+      context: 'is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
+      callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
+    },
+    location: {
+      name: 'Starcups',
+      address: '125 High Street, Reading, RG6 1PS',
+      rating: 3,
+      facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+      coords: {lat: -23.417796, lng: -51.935573},
+      openingTimes: [{
+        days: 'Monday - Friday',
+        opening: '7:00am',
+        closing: '7:00pm',
+        closed: false
+      },{
+        days: 'Saturday',
+        opening: '8:00am',
+        closing: '5:00pm',
+        closed: false
+      },{
+        days: 'Sunday',
+        closed: true
+      }],
+      reviews: [{
+        author: 'Evandro Oliveira',
+        rating: 5,
+        timestamp: '16 July 2013',
+        reviewText: 'What a great place. I can\'t say enough good things about it.'
+      },{
+        author: 'Paulo Fiscal Ricardo',
+        rating: 3,
+        timestamp: '16 June 2013',
+        reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
+      }]
+    }
+  });
 }
 
 /* Get Add Review Page */
 module.exports.addReview = function(req, res) {
-  res.render('location-review-form', { title: 'Add Review' });
+  res.render('location-review-form', {
+    title: 'Review Starcups on Loc8r',
+    pageHeader: {
+      title: 'Review Starcups'
+    }
+  });
 }
