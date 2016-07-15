b3c416869b96f3336744b40d336f6cf7c53ce348
Evandro Oliveira <eoliveira@kenzan.com>
2016-07-15 11:20:30 -0300

    Included api routes

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 require('./app_server/models/db')
 
 var routes = require('./app_server/routes/index');
+var routesApi = require('./app_api/routes/index');
+
 var users = require('./app_server/routes/users');
 
 var app = express();
@@ -24,6 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/api', routesApi);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
