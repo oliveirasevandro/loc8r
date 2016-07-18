89ff5f44ebacfba2b6e1f3b06993529ec317d247
Evandro Oliveira <eoliveira@kenzan.com>
2016-07-18 17:38:14 -0300

    Included endpoint read locations by distance

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -1,8 +1,46 @@
 var mongoose = require('mongoose');
 var Loc = mongoose.model('Location');
 
+var theEarth = (function () {
+  var earthRadius = 6371; //km, miles is 3959
+
+  function getDistanceFromRads(rads) {
+    return parseFloat(rads * earthRadius);
+  }
+
+  function getRadsFromDistance(distance) {
+    return parseFloat(distance / earthRadius);
+  }
+
+  return {
+    getDistanceFromRads: getDistanceFromRads,
+    getRadsFromDistance: getRadsFromDistance
+  };
+})();
+
 module.exports.locationsListByDistance = function (req, res) {
-  sendJsonResponse(res, 200, {status: 'success'});
+  var lng = parseFloat(req.query.lng);
+  var lat = parseFloat(req.query.lat);
+  var distance = parseInt(req.query.distance, 10) || 20;
+
+  console.log('lat: ' + lat + ' lng: ' + lng + ' distance: ' + distance);
+
+  var point = {
+    type: 'Point',
+    coordinates: [lng, lat]
+  };
+  var geoOptions = {
+    spherical: true,
+    maxDistance: theEarth.getRadsFromDistance(distance),
+    num: 10
+  };
+
+  Loc.geoNear(point, geoOptions, function (err, results) {
+    if (err) {
+      console.log('error:', err);
+    }
+    sendJsonResponse(res, 200, buildLocations(results));
+  });
 };
 
 module.exports.locationsCreate = function (req, res) {
@@ -40,4 +78,20 @@ module.exports.locationsDeleteOne = function (req, res) {
 
 function sendJsonResponse(res, status, content) {
   res.status(status).json(content);
+}
+
+function buildLocations(results) {
+  var locations = [];
+
+  results.forEach(function (doc) {
+    locations.push({
+      distance: theEarth.getDistanceFromRads(doc.dis),
+      name: doc.obj.name,
+      address: doc.obj.address,
+      rating: doc.obj.rating,
+      facilities: doc.obj.facilities,
+      _id: doc.obj._id
+    });
+  });
+  return locations;
 }
